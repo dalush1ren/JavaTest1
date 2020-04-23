f90b0107033e124dad6f58660001bd932983f5d1
dalush1ren <dalush1ren@163.com>
2020-04-23 15:45:51 +0800

    test

---
+++ untitled/src/test/demo1.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @Author: DLSR
+ * @Date: 2020/4/23 15:19
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
